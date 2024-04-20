a9b095b67b007b9be4fc916465efe4d7d16d88bc
Minsun Yoo <umsunny03@gmail.com>
2024-04-20 11:59:50 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,59 +1,33 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  const [dollar, setDollar] = useState(0);
-  const [index, setIndex] = useState(0);
-  const [coinNum, setCoinNum] = useState(0);
+const [loading, setLoading] = useState(true);
+const [movies, setMovies] = useState([]);
+const getMovies = async() => {
+  const json = await (await fetch(
+    "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+  )).json();
+  setMovies(json.data.movies);
+  setLoading(false);
+}
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
+    getMovies();
   }, []);
-
-  const onInputChange = (event) => {
-    setDollar(event.target.value);
-    getCoinNum();
-  };
-
-  const onSelectChange = (event) => {
-    setIndex(event.target.value);
-    getCoinNum();
-  };
-
-  function getCoinNum() {
-    const price = coins[index].quotes.USD.price;
-    const result = Math.round(dollar / price * 100) / 100;
-    if(result !== NaN && result > 0) {
-      setCoinNum(result);
-    }
-  }
-
   return (
     <div>
-      <h1>The Coins ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : 
-        <div>          
-            If you buy{" "}
-            <select onChange={onSelectChange}>
-            {coins.map((coin, index) => (
-              <option key={coin.id} value={index}>          
-                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
-              </option>
-            ))}
-            </select>
-            <br/>
-            with{" "}
-            <input id="dollar" onChange={onInputChange} placeholder="input dollars..."/> dollars
-          <hr />
-          You can get <input id="coinNum" value={coinNum} readOnly={true}/> coins
+      {loading ? <h1>Loading</h1> : 
+        <div>
+          {movies.map((movie) => ( 
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}/>
+              <h1>{movie.title}</h1>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => <li key={g}>{g}</li>)}
+              </ul>
+            </div>))}
         </div>
-      } 
+      }
     </div>
   )
 }
